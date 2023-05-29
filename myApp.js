a0f47044a63104e995246e981136e9b2e9d0df9a
Hirok Sengupta <senguptahirok@gmail.com>
2023-05-29 10:16:39 +0530

    added process.env

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 require('mongoose');
 require('dotenv').config();
-console.log('MONGO_URI = '+ MONGO_URI);
+console.log('MONGO_URI = '+ process.env.MONGO_URI);
 
-mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true});
 
 let Person;
 
