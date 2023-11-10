728b35ad1f56fd278fa41ebfeae5dd47c4877e39
Hirok Sengupta <senguptahirok@gmail.com>
2023-11-10 13:29:36 +0530

    debugging

--- myApp.js
+++ myApp.js
@@ -29,9 +29,9 @@ const createAndSavePerson = (done) => {
 /*    else 
       console.log('data = ' + data); */
     console.log('*** leaving function save ***');
-    done(null, data);
+//    done(null, data);
   })
-// done(null /*, data*/);
+ done(null /*, data*/);
 };
 /*
 let arrayOfPeople = [{name: 'ashim', age: 40, favoriteFoods: ['mutton', 'rice']},
