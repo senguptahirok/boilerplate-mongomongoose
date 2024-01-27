d3dc7184d04d690949cba7d35c94ff9f104c8e5e
Hirok Sengupta <senguptahirok@gmail.com>
2024-01-27 13:07:37 +0530

    debugging

--- myApp.js
+++ myApp.js
@@ -68,7 +68,7 @@ const findOneByFood = (food, done) => {
     if (err)
       console.log('error = ' + err);
     else 
-      console.log('data was found = '+ data)
+      console.log('data was found = '+ data[age]);
     done(null, data);
   })
   //done(null /*, data*/);
