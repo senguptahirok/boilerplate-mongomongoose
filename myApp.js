1b13796d832a449584a1b0b45f1f1997a3d651f0
Hirok Sengupta <senguptahirok@gmail.com>
2024-02-28 16:50:11 +0530

    debugging

--- myApp.js
+++ myApp.js
@@ -154,7 +154,7 @@ const removeManyPeople = (done) => {
     done(null, data);
     console.log('after done in remove');
   })
-//  done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const queryChain = (done) => {
@@ -166,9 +166,9 @@ const queryChain = (done) => {
   .exec(function(err,data){
     if (err) console.log('error msg, not found - '+err);
     else console.log('person was found with food '+foodToSearch+', person is :'+data);
-//    done(err,data);
+    done(err,data);
   });
-  done(null /*, data*/);
+//  done(null /*, data*/);
 };
 
 /** **Well Done !!**
